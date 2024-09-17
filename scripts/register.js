36d5802560e19ff26b18c2e8e8e1827bc815d395
Faiza Abdul-Wahab <faizaabdulwahab@ymail.com>
2024-09-17 16:04:03 +0000

    jsupdate

--- scripts/register.js
+++ scripts/register.js
@@ -1,9 +1,27 @@
 //Get form element by id
-const registerForm = document.getElementById('register');
-
+const registerForm = document.querySelector('#register');
 //Handle form submit event
-registerForm.onsubmit = function(event) {
-    event.preventDefault();
-    console.log(event)
+registerForm.addEventListener('submit',function (event) {
+    event.preventDefault(); 
+    // collect form data
+    const formData = new FormData(registerForm);
+    // console.log(
+    //     formData.get('username'),
+    //     formData.get('email'), 
+    //     formData.get('password')
+    // );
+    // send data to backens
+    // send success message to user
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent ='Account registered sucessfully';
+    
+    });
 
+/* declare variable age*/
+const age = 71;
+//define square age function
+function squareAge (age){
+   return age ** 2;   
 }
+const squaredAge = squareAge(54);
+console.log(squaredAge);
